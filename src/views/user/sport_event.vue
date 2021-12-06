06013f76dc75af17192471a90b962d9209444a08
luxinru <865666015@qq.com>
2021-12-06 16:00:36 +0800

    fix: 修改运动健康UI

--- src/views/user/sport_event.vue
+++ src/views/user/sport_event.vue
@@ -1,45 +1,40 @@
 <template>
   <div class="main">
     <section class="header">
-      <van-icon class="iconLeft" name="arrow-left" @click="$router.back()" />
+      <van-icon
+        class="iconLeft"
+        name="arrow-left"
+        color="#fff"
+        @click="$router.back()"
+      />
     </section>
-    <section class="title">运动加油站</section>
-    <div class="lists-box" v-for="(item, key) in data" :key="key">
-      <div class="lists">
+
+    <div class="lists-box">
+      <div class="lists" v-for="(item, key) in data" :key="key">
         <img :src="item.image" class="iocs" />
         <div class="info">
-          <div class="info_title">{{ item.title }}</div>
-
-          <div class="btns">
-            <div class="btns_info">
-              <van-icon
-                name="clock-o"
-                color="rgba(255, 255, 255, 1)"
-                size="16px"
-              />
-              <span>完成时间{{ item.date }}分钟</span>
-            </div>
-            <div
-              class="buts actions"
-              v-if="item.status == '0'"
-              @click="takeExercises(item.id)"
-            >
-              {{ item.status_text }}
-            </div>
-            <div
-              class="buts receive"
-              v-else-if="item.status == '1'"
-              @click="receiveRewardsExercises(item.id)"
-            >
-              {{ item.status_text }}
-            </div>
-            <div class="buts noaction" v-else-if="item.status == '2'">
-              {{ item.status_text }}
-            </div>
-            <div class="buts nowaction" v-else-if="item.status == '3'">
-              {{ item.status_text }}
-            </div>
-          </div>
+          <div class="info_title">{{ item.title }} <span>+10积分</span></div>
+          <span class="info_value">运动{{ item.date }}分钟</span>
+        </div>
+        <div
+          class="but actions"
+          v-if="item.status == '0'"
+          @click="takeExercises(item.id)"
+        >
+          {{ item.status_text }}
+        </div>
+        <div
+          class="but receive"
+          v-else-if="item.status == '1'"
+          @click="receiveRewardsExercises(item.id)"
+        >
+          {{ item.status_text }}
+        </div>
+        <div class="but noaction" v-else-if="item.status == '2'">
+          {{ item.status_text }}
+        </div>
+        <div class="but nowaction" v-else-if="item.status == '3'">
+          {{ item.status_text }}
         </div>
       </div>
     </div>
@@ -47,127 +42,125 @@
 </template>
 
 <script>
-import Fetch from "../../utils/fetch";
+import Fetch from '../../utils/fetch'
 
 export default {
-  name: "index",
+  name: 'index',
   components: {},
-  data() {
+  data () {
     return {
       data: {},
       minutes: 0, //分
       seconds: 0, //秒
       status: false,
-      action: false,
-    };
+      action: false
+    }
   },
-  created() {
-    this.$parent.footer(false);
+  created () {
+    this.$parent.footer(false)
   },
   methods: {
-    start() {
-      Fetch("/index/getSportEventList").then((res) => {
-        this.data = res.data.list;
-        this.minutes = res.data.m;
-        this.seconds = res.data.s;
+    start () {
+      Fetch('/index/getSportEventList').then(res => {
+        this.data = res.data.list
+        this.minutes = res.data.m
+        this.seconds = res.data.s
         if (this.seconds > 0 || this.minutes > 0) {
-          this.seconds = this.seconds + 1;
-          this.status = true;
+          this.seconds = this.seconds + 1
+          this.status = true
           if (this.action == false) {
-            this.timer(true);
+            this.timer(true)
           }
         } else {
-          this.action = false;
+          this.action = false
         }
-      });
+      })
     },
-    takeExercises(id) {
-      Fetch("/index/takeExercises", { sid: id }).then((res) => {
+    takeExercises (id) {
+      Fetch('/index/takeExercises', { sid: id }).then(res => {
         this.$notify({
-          background: "#07c160",
-          message: res.info,
-        });
-        this.start();
-      });
+          background: '#07c160',
+          message: res.info
+        })
+        this.start()
+      })
     },
-    receiveRewardsExercises(id) {
-      Fetch("/index/receiveRewardsExercises", { sid: id }).then((res) => {
+    receiveRewardsExercises (id) {
+      Fetch('/index/receiveRewardsExercises', { sid: id }).then(res => {
         this.$notify({
-          background: "#07c160",
-          message: res.info,
-        });
-        this.start();
-      });
+          background: '#07c160',
+          message: res.info
+        })
+        this.start()
+      })
     },
     // 倒计时
-    num(n) {
+    num (n) {
       if (this.minutes === 0 && this.seconds === 0 && this.status == true) {
-        this.start();
-        this.status = false;
+        this.start()
+        this.status = false
       }
-      return n < 10 ? "0" + n : "" + n;
+      return n < 10 ? '0' + n : '' + n
     },
     // 倒计时
-    timer(is_action = false) {
+    timer (is_action = false) {
       if (is_action) {
-        this.action = true;
+        this.action = true
       }
-      var _this = this;
+      var _this = this
       var time = window.setInterval(function () {
         if (_this.seconds === 0 && _this.minutes !== 0) {
-          _this.seconds = 59;
-          _this.minutes -= 1;
+          _this.seconds = 59
+          _this.minutes -= 1
         } else if (_this.minutes === 0 && _this.seconds === 0) {
-          _this.seconds = 0;
-          window.clearInterval(time);
+          _this.seconds = 0
+          window.clearInterval(time)
         } else {
-          _this.seconds -= 1;
+          _this.seconds -= 1
         }
-      }, 1000);
-    },
+      }, 1000)
+    }
   },
-  mounted() {
+  mounted () {
     // 倒计时
-    this.start();
+    this.start()
   },
   watch: {
     // 倒计时
     second: {
-      handler(newVal) {
-        this.num(newVal);
-      },
+      handler (newVal) {
+        this.num(newVal)
+      }
     },
     // 倒计时
     minute: {
-      handler(newVal) {
-        this.num(newVal);
-      },
-    },
+      handler (newVal) {
+        this.num(newVal)
+      }
+    }
   },
   computed: {
     // 倒计时
     second: function () {
-      return this.num(this.seconds);
+      return this.num(this.seconds)
     },
     minute: function () {
-      return this.num(this.minutes);
-    },
-  },
-};
+      return this.num(this.minutes)
+    }
+  }
+}
 </script>
 
 <style scoped lang="less">
 .main {
   width: 100%;
-  min-height: 343px;
-  padding-bottom: 100px;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background: url(~@/assets/images/运动加油站_slices/bg-运动加油站@2x.png)
-    no-repeat;
-  background-size: 100% 343px;
-  background-color: #fff;
+  background: url('~@/assets/wanheng/运动ui_slices/组 32@2x.png') no-repeat;
+  background-size: 100% 812px;
+  overflow: scroll;
 
   .header {
     width: 100%;
@@ -177,111 +170,95 @@ export default {
     padding: 0 16px;
     box-sizing: border-box;
   }
-
-  .title {
-    width: 100%;
-    font-size: 28px;
-    font-family: PingFang SC-Semibold, PingFang SC;
-    font-weight: 600;
-    color: #000000;
-    line-height: 33px;
-    margin: 11px 0 0 0;
-    padding: 0 16px;
-    box-sizing: border-box;
-  }
 }
 .lists-box {
-  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 338px;
+  background: #ffffff;
+  border-radius: 7px;
+  padding: 40px 16px;
+  margin: 337px auto 24px;
 }
 .lists {
   position: relative;
-  width: 346px;
-  height: 120px;
-  border-radius: 15px;
-  box-sizing: border-box;
-  margin-top: 17px;
-  overflow: hidden;
-  padding: 16px;
-  box-sizing: border-box;
+  width: 100%;
+  height: 40px;
+  margin-top: 42px;
+  display: flex;
+  align-items: center;
+
+  &:first-child {
+    margin-top: 0;
+  }
 }
 .iocs {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  left: 0;
-  top: 0;
+  width: 40px;
+  height: 40px;
+  margin-right: 11px;
+  border-radius: 50%;
 }
 
 .info {
+  flex: 1 0;
+  height: 100%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
 
   .info_title {
-    width: 100%;
-    font-size: 23px;
-    font-family: PingFang SC-Semibold, PingFang SC;
-    font-weight: 600;
-    color: #ffffff;
-    line-height: 27px;
-    text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
-  }
+    font-size: 15px;
+    font-family: PingFang SC;
+    font-weight: 400;
+    color: #393939;
 
-  .btns {
-    width: 100%;
-    height: 34px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-top: 19px;
-
-    .btns_info {
-      display: flex;
-      align-items: center;
-      span {
-        font-size: 16px;
-        font-family: PingFang SC-Regular, PingFang SC;
-        font-weight: 400;
-        color: rgba(255, 255, 255, 0.6);
-        margin-left: 4px;
-      }
+    span {
+      font-size: 13px;
+      font-weight: 400;
+      color: #F5763D;
+      margin-left: 24px;
     }
   }
+
+  .info_value {
+    font-size: 13px;
+    font-weight: 400;
+    color: #B5B5B5;
+    margin-top: 11px;
+  }
 }
-.buts {
-  width: 110px;
-  height: 34px;
-  background: #ff8a35;
-  border-radius: 35px;
+.but {
   display: flex;
   align-items: center;
   justify-content: center;
+  background: #ff8a35;
+  border-radius: 11px;
+  padding: 6px 13px;
 }
 .actions {
   //background-color: #FFFFFF;
-  font-size: 16px;
+  font-size: 13px;
   font-family: PingFang SC-Semibold, PingFang SC;
   font-weight: 500;
   color: #ffffff;
 }
 .noaction {
   background-color: rgba(204, 204, 204, 1);
-  font-size: 16px;
+  font-size: 13px;
   font-family: PingFang SC-Semibold, PingFang SC;
   font-weight: 500;
   color: #808080;
 }
 .receive {
   background-color: rgba(50, 50, 50, 1);
-  font-size: 16px;
+  font-size: 13px;
   font-family: PingFang SC-Semibold, PingFang SC;
   font-weight: 500;
   color: #ff8a35;
 }
 .nowaction {
-  font-size: 16px;
+  font-size: 13px;
   font-family: PingFang SC-Semibold, PingFang SC;
   font-weight: 500;
   background-color: #808a87;
